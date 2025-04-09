4b56bfa6a7d5020220360d79f0806cee215df290
RISHYAVANDHAN <rishyavandhan.v@gmail.com>
2025-04-09 15:08:20 +0200

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="particle-simulator",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "taichi",
+        "numpy",
+        # Add other dependencies as needed
+    ],
+    description="Particle physics simulation package",
+    python_requires=">=3.6",
+)
